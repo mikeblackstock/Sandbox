fc354f4208d0edc8ae1bc533079f8eeb1bd61e69
Mike Blackstock <mike (at) omet.ca>
2019-10-14 16:58:40 -0400

    fixed a config

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,30 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 const mode = process.env.NODE_ENV || 'development';
 const minimize = mode === 'production';
-const plugins = [];
-
-if (mode === 'production') {
-  plugins.push(new OptimizeCSSAssetsPlugin({
-    cssProcessorOptions: {
-      discardComments: true
-    },
-  }));
-}
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode,
   devtool: 'source-map',
   entry: [
-    path.resolve(__dirname, 'index.js')
+    path.resolve(__dirname, 'index.js'),
   ],
-  externals: {
-    osjs: 'OSjs'
-  },
   optimization: {
     minimize,
   },
+  externals: {
+    osjs: 'OSjs'
+  },
   plugins: [
     new CopyWebpackPlugin([
-      path.resolve(__dirname, 'icon.png')
-    ]),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
-    ...plugins,
+      'icon.png'
+    ])
   ],
   module: {
     rules: [
       {
-        test: /\.(sa|sc|c)ss$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              minimize,
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules\/(?!@osjs)/,
         use: {
           loader: 'babel-loader'
         }
